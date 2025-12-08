eb53b88fa921ec39579cded34ebfe4375df80960
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2025-12-08 19:15:29 +0400

    fixed bugs

--- app/next-sitemap.config.ts
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://puti-nn.ru", // замените на ваш домен
-    generateRobotsTxt: true, // создаст robots.txt автоматически
-};
