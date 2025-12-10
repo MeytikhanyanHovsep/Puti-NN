db59252d626ed81986bc026e3d437c3d5bb58335
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2025-12-10 15:22:37 +0400

    fixing bugs

--- components/index.tsx
+++ components/index.tsx
@@ -6,7 +6,14 @@ type Props = {
     children: React.ReactNode;
 };
 
+import { usePathname } from "next/navigation";
+import NotFound from "@/app/not-found";
+
 export default function Components({ children }: Props) {
+    const pathname = usePathname();
+    const knownPaths = ["/", "/about", "/api/send-to-email"];
+
+    if (!knownPaths.includes(pathname)) return <NotFound />;
     const menuItems: any = {
         about: "О-Нас",
         services: "Услуги",
